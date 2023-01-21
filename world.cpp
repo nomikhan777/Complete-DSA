08cacd8d31f165a2c6871db06849b48cd4045721
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-01-21 18:10:03 +0500

    my commit

---
+++ world.cpp
@@ -0,0 +1,14 @@
+// C++ program to display "Hello World"
+
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+
+// Main() function: where the execution of program begins
+int main()
+{
+    // prints hello world
+    cout << "hello World"<<1+2;
+
+    return 0;
+}
