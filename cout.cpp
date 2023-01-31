0bcb519e6e5544d9d470abe5f54e61af3a7e1f06
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-01-31 11:12:18 +0500

    my commit

---
+++ cout.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //  Cout implementation
+    // cout<<"Programming is "<<"great fun!";
+  
+    // Another Way
+    cout << "Programming is";
+    cout << " Great Fun";
+
+    
+
+    return 0;
+}
---
+++ endl.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << " The following items were top sealers " << endl;
+    cout << " during the month of June:" << endl;
+    cout << " Computer Games " << endl;
+    cout << " Coffee " << endl;
+    cout << " Aspirian " << endl;
+
+    return 0;
+}
