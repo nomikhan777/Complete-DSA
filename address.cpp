5346e9bf8246331ae43b30734377a8ba17e36f7a
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-02-02 10:46:16 +0500

    my commit

---
+++ address.cpp
@@ -0,0 +1,10 @@
+// Dated Feb 2 
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Muhammad Noman\n ";
+    cout << "Street#9\n";
+    cout << "Burewala Punjab 61010\n";
+    cout << "03176525905 \n";
+}
---
+++ program1.cpp
@@ -0,0 +1,11 @@
+// The Works of Wolfgang
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "The works of Wolfgang\ninclude the following";
+    cout << "\nThe Turkish March" << endl;
+    cout << "and Symphony No. 40 ";
+    cout << "in G minor." << endl;
+    return 0;
+}
