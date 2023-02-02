aa6e995aa8f13260d0ed8114e2def9db5d94d848
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-02-02 10:13:16 +0500

    my commit

---
+++ n/n.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << " The folllowing Items were top sellers \n";
+    cout << " During the month of June:\n";
+    cout << " Computer Games \n Coffee \n";
+    cout << " Aspirian \n ";
+}
