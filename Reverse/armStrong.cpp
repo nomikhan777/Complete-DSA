5267f0f137c526685bba6ecebb7b0863ba67d625
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-03-02 16:13:08 +0500

    2nd March

---
+++ Reverse/armStrong.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    int originaln = n;
+
+    while (n > 0)
+    {
+        int lastdigit = n % 10;
+        sum += pow(lastdigit, 3);
+        n = n / 10;
+    }
+
+    if (sum == = original)
+    {
+        cout << "Number is ArmStrong" << endl;
+    }
+    else
+    {
+        cout << "Number is not ArmStrong"
+    }
+
+    return 0;
+}
