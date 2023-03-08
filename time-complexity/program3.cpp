455f2516d708f737fd2fb7423dfda27150a63e14
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-03-08 10:11:54 +0500

    8th march

---
+++ time-complexity/program3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int a = 0;
+    int i=n;
+    while (i >= 1)
+    {
+        a = a + 1;
+        i /=2;
+    }
+
+    
+    
+}
