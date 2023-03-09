6db88f863e5b3f260284a8a6cae3a301476b4ddf
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-03-09 10:03:40 +0500

    9th march

---
+++ Arrays/program.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+     
+
+     int array[4] = {10,20,30,40};
+    //  array[0] =  10;
+    //  array[1] = 20;
+    //  array[2] = 30;
+    //  array[3] = 40;
+
+cout<<array[3]<<endl;
+
+
+
+    return 0;
+
+}
