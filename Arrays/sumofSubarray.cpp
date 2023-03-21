f1aee79367d3950219d673cdba5ddc1feebede50
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-03-21 14:32:56 +0500

    two google QS

--- Arrays/sumofSubarray.cpp
+++ Arrays/sumofSubarray.cpp
@@ -2,7 +2,26 @@
 using namespace std;
 int main(){
     int n;
-    cin>>n;
+    cin >> n;
+    int arr[n];
+
+    for (int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int current = 0;
+    for (int i=0; i<n; i++)
+    {
+        current = 0;
+        for (int j=i; j<n ; j++)
+        {
+            current +=arr[j];
+            cout << current <<endl;
+        }
+        
+    }
+    
     
 
 
