40a767526545bb24af1c15f21f90601c7c0b92a2
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-04-05 15:23:43 +0500

    Palindrome imp

---
+++ Arrays/Palindrome.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // Check Palindrome
+    int n;
+    cin>>n;
+    char arr[n+1];
+    cin>>arr;
+
+    bool check = 1;
+
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] !=arr[n-1-i])
+        {
+            check = 0;
+            break;
+        }
+        
+    }
+
+
+    if (check == true)
+    {
+        cout<<"word is a palindrome"<<endl;
+    }
+
+    else{
+        cout<<"word is not palindrome" <<endl;
+    }
+    
+    
+
+return 0;
+
+
+
+}
