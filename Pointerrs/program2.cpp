57f96e92a8aa4bc108e56059e134852001e307b8
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-04-06 14:35:32 +0500

    POnters

---
+++ Pointerrs/program2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // POinters are used to store the addresses of variables
+
+
+
+    // int a = 10;
+    // int* aptr = &a;
+    // cout<<*aptr<<endl;
+
+    // *aptr = 20;
+    // cout<<a<<endl;
+
+    int arr[]={10,20,30};
+    cout<<*arr<<endl;
+
+
+
+
+    return 0;
+}
