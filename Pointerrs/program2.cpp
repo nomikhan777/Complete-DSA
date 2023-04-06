1bd05748e8523268e4b29f14b972133e194604d1
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-04-06 14:38:50 +0500

    POnters

--- Pointerrs/program2.cpp
+++ Pointerrs/program2.cpp
@@ -15,7 +15,13 @@ int main(){
     int arr[]={10,20,30};
     cout<<*arr<<endl;
 
-
+    int *ptr = arr;
+    for (int i = 0; i < 3; i++)
+    {
+        cout<<*ptr<<endl;
+        ptr++;
+    }
+    
 
 
     return 0;
